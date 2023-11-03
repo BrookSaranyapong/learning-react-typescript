8a8e4d288df17f1e1c842edfe55fad3a485020d0
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-11-04 00:27:14 +0700

    update AppFooter

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,9 +1,16 @@
 function AppFooter() {
   const company = "Brook Saranyapong";
+  const isShow = false;
+
+  const showMsg = () => {
+    alert("Hello TypeScript");
+  };
   return (
     <>
+      <button onClick={showMsg}>Click Me!</button>
       <p>created by {company}</p>
       <p>{new Date().getFullYear()}</p>
+      {isShow && <p>Hello React 18</p>}
     </>
   );
 }
