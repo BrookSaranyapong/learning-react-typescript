e778735625e99078ee208f4b7b3ed00e60b2b4b1
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-11-04 14:27:14 +0700

    Update add React Router Dom v6

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,19 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./global.css";
 import HomePage from "./pages/home-page.tsx";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <HomePage />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
