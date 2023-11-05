76d7a36d1b13775edceaf7cc2e5ae1a0bba12662
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-11-05 20:26:18 +0700

    Update

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { LoginResponse } from "../interfaces/login.type";
+import { ProfileResponse } from "../interfaces/profile.type";
 import { AxiosResponse, http } from "./http.service";
 
 
@@ -13,6 +14,12 @@ export function logout(): void {
     localStorage.removeItem('token');
 }
 
-export function getProfile() {
-
+export async function getProfile(): Promise<AxiosResponse<ProfileResponse> | null> {
+    const token = JSON.parse(localStorage.getItem('token')!) as LoginResponse;
+    if (!token) {
+        return null;
+    }
+    return await http.get<ProfileResponse>('https://api.codingthailand.com/api/profile',{
+        headers : { Authorization : `Bearer ${token.access_token}`}
+    })
 }
