c20f6a938c65b0ee3b1616fbf1162305811abd48
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-11-05 22:04:43 +0700

    fixed bug week and not Working big-calendar

--- src/pages/dashboard/d-home.tsx
+++ src/pages/dashboard/d-home.tsx
@@ -11,6 +11,7 @@ import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import { th } from "date-fns/locale";
+import { Event } from "../../interfaces/room-booking.type";
 
 const locales = {
   "th-TH": th,
@@ -33,22 +34,47 @@ function DHome() {
   }, []);
 
   const onSelectEvent = (caleEvent: any) => {
-    alert(JSON.stringify(caleEvent));
+    const data = {
+      id: caleEvent.id,
+      title: caleEvent.title,
+      start: format(Date.parse(caleEvent.start), "dd MMM yyyy HH:mm:ss", {
+        locale: th,
+      }),
+      end: format(Date.parse(caleEvent.end), "dd MMM yyyy HH:mm:ss", {
+        locale: th,
+      }),
+      createdBy: caleEvent.created_by,
+    };
+    alert(JSON.stringify(data));
   };
+
+  let myEvents = roomBooking?.events.map((item: Event) => {
+    return {
+      id: item.id,
+      title: item.title,
+      start: new Date(item.start),
+      end: new Date(item.end),
+      created_by: new Date(item.created_by),
+    };
+  });
+
   return (
     <>
       <h1>รายการจองห้องประชุมทั้งหมด</h1>
       <Calendar
         culture="th-TH"
         localizer={localizer}
-        events={[
-          ...(roomBooking?.events != undefined ? roomBooking.events : []),
-        ]}
+        events={[...(myEvents != undefined ? myEvents : [])]}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 600 }}
         onSelectEvent={onSelectEvent}
-        views={['month']}
+        views={["day", "week", "month", "agenda"]}
+        formats={{
+          dateFormat: "d",
+          monthHeaderFormat: "dd MM yyyy",
+          timeGutterFormat: "HH:mm",
+        }}
       />
     </>
   );
