44e32db554e2473a831521fd41b51ce6a65e9271
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-11-05 13:41:22 +0700

    Update Axios Auth.services

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,9 +1,11 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const http = axios.create({
-    headers:{
-        'Content-Type':'application/json'
+    headers: {
+        'Content-Type': 'application/json'
     }
 });
 
 export { http };
+
+export type { AxiosResponse }
